306747833a0bd9f6da378c6d949d4e5afd3318b9
Patryk <mastasinqwer@gmail.com>
2020-10-26 14:16:13 +0100

    heroku

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
-import React from 'react';
-import Routes from './routes/routes';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Loader from './components/loader/loader'
 
-const App = () => <Routes/>
+const Nav = lazy(() => import('./components/nav/nav'));
+// const CrumbsBread = lazy(() => import('../components/crumbs-bread/crumbs-bread'));
+
+const Cart = lazy(() => import('./pages/cart/cart'));
+const Home = lazy(() => import('./pages/home/home'));
+const Login = lazy(() => import('./pages/login/login'));
+const ProductDetail = lazy(() => import('./pages/product-detail/product-detail'));
+const Shop = lazy(() => import('./pages/shop/shop'));
+const Profile = lazy(() => import('./pages/profile/profile'));
+
+const Footer = lazy(() => import('./components/footer/footer'));
+
+const Page404 = lazy(() => import('./pages/page404/page404'));
+
+const App = () => (
+    <Router>
+        <Suspense
+            fallback={<Loader />}
+        >
+            <Nav/>
+            {/* <CrumbsBread/> */}
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path='/cart' component={Cart} />
+                <Route path='/login' component={Login} />
+                <Route exact path='/shop' component={Shop} />
+                <Route path='/shop/:id'  component={ProductDetail} />
+                <Route path='/profile' component={Profile} />
+
+                <Route path='*' exact={true} component={Page404} />
+            </Switch>
+            <Footer/>
+        </Suspense>
+    </Router>
+);
 
 export default App;

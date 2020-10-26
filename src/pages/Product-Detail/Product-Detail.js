2587d8c9daec67fbc3e88df56e8c3fa5e4cfe0cc
Patryk <mastasinqwer@gmail.com>
2020-10-26 14:13:06 +0100

    heroku

--- src/pages/Product-Detail/Product-Detail.js
+++ src/pages/Product-Detail/Product-Detail.js
@@ -7,13 +7,13 @@ import { Link } from 'react-router-dom';
 const ProductDetail = ({ match }) => {
     const [item, setItem] = useState({});
     const { id, title, price, description, category } = item;
+    const Image = `${process.env.PUBLIC_URL}./assets/products/${id}-min.png`
 
     useEffect(() => {
         ITEMS.filter(item => parseInt(match.params.id) === item.id && setItem(item));
     })
     if (match.params.id >= ITEMS.length + 1) return <div style={{minHeight: '80vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>We don't have that products, check url</div>
 
-    const Image = `${process.env.PUBLIC_URL}./assets/products/${id}-min.png`
     return (
         <div className='p-detail'>
             <Link to='/shop' className='p-detail__btn'>Back to shop</Link>
